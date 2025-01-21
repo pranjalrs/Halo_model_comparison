a2bfe7349e2bc4f90e5fc1f62f7256cd31be0349
Pranjal RS <rs.pranjal12@gmail.com>
2025-01-21 12:32:48 -0700

    added computation of profile scatter

--- src/simulation_profiles.py
+++ src/simulation_profiles.py
@@ -11,7 +11,7 @@ def mass_integral(r, rho):
 
 files = glob.glob('../magneticum-data/data/profiles/Box2/hr_bao_cdm*nhalo2000.pkl')
 
-profile_handler = HaloProfileHandler(['matter', 'cdm', 'gas', 'star'], files)
+profile_handler = HaloProfileHandler(['matter', 'cdm', 'gas', 'Pe', 'star'], files)
 
 
 
@@ -23,17 +23,19 @@ def get_binned_profiles(m_min, m_max, n_bins):
 
 	mean_profiles_matter, mean_profiles_cdm = [], []
 	mean_profiles_gas, mean_profiles_star = [], []
+	mean_profiles_Pe = []
 
 	mean_rvirs, mean_mvirs  = [], []
 	Nhalos = []
 
 	for i in range(len(mass_bin_edges)-1):
 		mmin, mmax = mass_bin_edges[i], mass_bin_edges[i+1]
-		rmin = 10  # in kpc/h
+		rmin = 0  # in kpc/h
 
-		cdm_profs = profile_handler.get_masked_profile(mmin, mmax, 20, 'cdm')
-		gas_profs = profile_handler.get_masked_profile(mmin, mmax, 20, 'gas')
-		star_profs = profile_handler.get_masked_profile(mmin, mmax, 20, 'star')
+		cdm_profs = profile_handler.get_masked_profile(mmin, mmax, rmin, 'cdm')
+		gas_profs = profile_handler.get_masked_profile(mmin, mmax, rmin, 'gas')
+		Pe_profs = profile_handler.get_masked_profile(mmin, mmax, rmin, 'Pe')
+		star_profs = profile_handler.get_masked_profile(mmin, mmax, rmin, 'star')
 
 		mean_rvir = np.average(cdm_profs.rvir, weights=cdm_profs.mvir)/1e3  # Convert to Mpc/h
 
@@ -44,23 +46,32 @@ def get_binned_profiles(m_min, m_max, n_bins):
 		cdm_mean_prof = np.nanmean(cdm_profs.profile, axis=0)
 		cdm_mean_r = np.nanmean(cdm_profs.rbins, axis=0)
 		cdm_mean_x = np.nanmean(cdm_profs.xbins, axis=0)
+		cdm_scatter = profile_handler.get_scatter(cdm_profs.profile, cdm_mean_prof)
 
 		gas_mean_prof = np.nanmean(gas_profs.profile, axis=0)
 		gas_mean_r = np.nanmean(gas_profs.rbins, axis=0)
 		gas_mean_x = np.nanmean(gas_profs.xbins, axis=0)
+		gas_scatter = profile_handler.get_scatter(gas_profs.profile, gas_mean_prof)
+
+		Pe_mean_prof = np.nanmean(Pe_profs.profile, axis=0)
+		Pe_mean_r = np.nanmean(Pe_profs.rbins, axis=0)
+		Pe_mean_x = np.nanmean(Pe_profs.xbins, axis=0)
+		Pe_scatter = profile_handler.get_scatter(Pe_profs.profile, Pe_mean_prof)
 
 		star_mean_prof = np.nanmean(star_profs.profile, axis=0)
 		star_mean_r = np.nanmean(star_profs.rbins, axis=0)
 		star_mean_x = np.nanmean(star_profs.xbins, axis=0)
+		star_scatter = profile_handler.get_scatter(star_profs.profile, star_mean_prof)
 
 		matter_mean_prof = cdm_mean_prof + gas_mean_prof + star_mean_prof
 		matter_mean_r = (cdm_mean_prof*cdm_mean_r +  gas_mean_prof* gas_mean_r + star_mean_prof*star_mean_r)/matter_mean_prof
 		matter_mean_x = (cdm_mean_prof*cdm_mean_x +  gas_mean_prof* gas_mean_x + star_mean_prof*star_mean_x)/matter_mean_prof
 
 		mean_profiles_matter.append([matter_mean_prof, matter_mean_r, matter_mean_x])
-		mean_profiles_cdm.append([cdm_mean_prof, cdm_mean_r, cdm_mean_x])
-		mean_profiles_gas.append([gas_mean_prof, gas_mean_r, gas_mean_x])
-		mean_profiles_star.append([star_mean_prof, star_mean_r, star_mean_x])
+		mean_profiles_cdm.append([cdm_mean_prof, cdm_mean_r, cdm_mean_x, cdm_scatter])
+		mean_profiles_gas.append([gas_mean_prof, gas_mean_r, gas_mean_x, gas_scatter])
+		mean_profiles_Pe.append([Pe_mean_prof, Pe_mean_r, Pe_mean_x, Pe_scatter])
+		mean_profiles_star.append([star_mean_prof, star_mean_r, star_mean_x, star_scatter])
 
 
 		mask_nan = np.isnan(matter_mean_prof)
@@ -76,8 +87,9 @@ def get_binned_profiles(m_min, m_max, n_bins):
 		mean_mvirs.append(mean_mvir)
 
 	mean_profiles_cdm = np.array(mean_profiles_cdm)
-	mean_profiles_gas = np.array(mean_profiles_gas)
+	mean_profiles_gas = np.array(mean_profiles_gas)  
+	mean_profiles_Pe = np.array(mean_profiles_Pe)*1e3 # in eV/cm^3h^2
 	mean_profiles_star = np.array(mean_profiles_star)
 	mean_profiles_matter = np.array(mean_profiles_matter)
 
-	return mean_profiles_matter, mean_profiles_cdm, mean_profiles_gas, mean_profiles_star, mean_rvirs, mean_mvirs, Nhalos
+	return mean_profiles_matter, mean_profiles_cdm, mean_profiles_gas, mean_profiles_Pe, mean_profiles_star, mean_rvirs, mean_mvirs, Nhalos
